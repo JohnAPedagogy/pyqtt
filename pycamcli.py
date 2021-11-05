e2ee1c3d9212aa1e4331bb1f4d5950a66fdefb62
Iyalla John Alamina <I.Alamina2@hud.ac.uk>
2021-11-05 06:59:46 +0000

    pcc01

--- pycamcli.py
+++ pycamcli.py
@@ -11,8 +11,13 @@ async def get_data(arg='localhost'):
         async with aiohttp.ClientSession(trust_env=True) as session:
             async with session.get(f'http://{arg}:8080/') as resp:
                 jpg_original = base64.b64decode(await resp.read())
+                if jpg_original==None:
+                    print("Nothing from server. Has the service been started?")
+                    break
                 nparr = np.frombuffer(jpg_original, np.uint8)
+                print(nparr)
                 img_np = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                print(img_np)
                 cv2.imshow('Output', img_np)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
